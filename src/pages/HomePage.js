7988cd436042d56e27a86978ec8f4eb81d99fc48
jayyyden <jaydennleemc@gmail.com>
2022-01-20 23:27:47 +0800

    added vector fonts lib & Home page update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default class HomePage extends Component {
   render() {
     return (
       <View style={styles.container}>
         <SafeAreaView />
+        <Text style={styles.location}>Hong Kong</Text>
         <View style={styles.header}>
           <Text style={styles.title}> Cocktails </Text>
+          <TouchableOpacity style={styles.searchContainer}>
+            <Icon name="search" size={24} color="#fff" />
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -24,9 +28,21 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between'
   },
+  location:{
+    color: '#6e6d6f',
+    fontSize: 16,
+    fontWeight:'800',
+    marginLeft: 12,
+  },
   title: {
     color: '#fff',
     fontSize: 35,
     fontWeight: 'bold',
+  },
+  searchContainer: {
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    borderRadius: 8,
+    backgroundColor: '#393939',
   }
 })
